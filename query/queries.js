e6a8511480d561e29d9a24d47d5f2fdf906f47db
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-15 13:08:29 +0200

    up to 3

--- query/queries.js
+++ query/queries.js
@@ -2,10 +2,10 @@
 db.restaurants.find({}, {_id:0});
 
 // 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
-
+db.restaurantes.find({}, {_id:0, restaurant_id: 1, name: 1});
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
-
+db.restaurantes.find({}, {_id:0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
 
 // 4. Mostrar restaurant_id, name, borough i zip code.
 
