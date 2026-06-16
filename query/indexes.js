fb040fc39dfb5c71baab5257b1b7058ea30a24fd
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-16 15:26:17 +0200

    Add multiple indexes to restaurants collection

--- query/indexes.js
+++ query/indexes.js
@@ -1,2 +1,8 @@
 //Use this file to define indexes for your queries
+db.restaurants.createIndex({ borough: 1 });
+db.restaurants.createIndex({ "grades.score": 1 });
+db.restaurants.createIndex({ cuisine: 1 });
+db.restaurants.createIndex({ name: 1 });
+db.restaurants.createIndex({ "address.street": 1 });
+db.restaurants.createIndex({ "location.coordinates": 1 });
 db.restaurants.createIndex({ location: "2dsphere" });
