70dfba16534a70ee6afce83a9cfbafafdb6fe49d
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-16 14:25:23 +0200

    fixed typos

--- query/queries.js
+++ query/queries.js
@@ -80,7 +80,7 @@ db.restaurants.find({}, {_id: 0}).sort({name: -1});
 db.restaurants.find({}, {_id: 0}).sort({cuisine: 1, borough: -1});
 
 // 28. Mostrar direccions que no contenen el carrer.
-db.restaurants.find({$or: [{"address.street": {$exists: false}}, {"adress.street": null}]]}, {_id: 0, address: 1});
+db.restaurants.find({$or: [{"address.street": {$exists: false}}, {"address.street": null}]}, {_id: 0, address: 1});
 
 // 29. Seleccionar documents on el valor de `coordinate` és de tipus Double. Mostrar el name, restaurant_id i coordinades.
 db.restaurants.find({"location.coordinates": {$elemMatch: {$type: "double" }}}, {_id: 0, restaurant_id: 1, "location.coordinates": 1});
