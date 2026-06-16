cf6bd1a1ab7da0c708ec9fd2995c92d4625153a9
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-16 13:34:43 +0200

    Fix formatting in queries.js for address.street query

--- query/queries.js
+++ query/queries.js
@@ -80,7 +80,7 @@ db.restaurants.find({}, {_id: 0}).sort({name: -1});
 db.restaurants.find({}, {_id: 0}).sort({cuisine: 1, borough: -1});
 
 // 28. Mostrar direccions que no contenen el carrer.
-db.restaurants.find({"address.street": {$exists: false}}, {_id: 0, address: 1});
+db.restaurants.find({"address.street": {$exists: false}}, {_id: 0, address: 1})
 
 // 29. Seleccionar documents on el valor de `coordinate` és de tipus Double. Mostrar el name, restaurant_id i coordinades.
 
