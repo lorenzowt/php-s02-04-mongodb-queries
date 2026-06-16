c546af4a83997e0819649fb7c6683eb8a20037a6
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-16 13:13:38 +0200

    added 23

--- query/queries.js
+++ query/queries.js
@@ -62,10 +62,10 @@ db.restaurants.find({"grades.score": {$lte: 10}}, {_id: 0, restaurant_id: 1, nam
 db.restaurants.find({$or : [{cuisine: "Seafood"}, {name: {$regex: "^Wil"}}]}, {_id: 0});
 
 // 22. Trobar restaurant_id, name, i grades per grau "A", score 11, i data "2014-08-11T00:00:00Z".
-db.restaurants.find({grades: {$elemMatch: {grade: "A", score: 11, date: "2014-08-11T00:00:00.000Z"}}},{_id: 0, restaurant_id: 1, name: 1, grades: 1});
+db.restaurants.find({grades: {$elemMatch: {grade: "A", score: 11, date: "2014-08-11T00:00:00Z"}}}, {_id: 0, restaurant_id: 1, name: 1, grades: 1});
 
 // 23. Trobar restaurant_id, name i grades on el 2n element té grau "A", score 9 i data "2014-08-11T00:00:00Z".
-
+db.restaurants.find({grades.1: {grade: "A", score: 9, date: "2014-08-11T00:00:00Z"}}, {_id: 0, restaurant_id: 1, name: 1, grades: 1})
 
 // 24. Trobar el restaurant_id, name, street, zipcode i coordenades dels restaurants a menys de 5 km de [-74, 40.7].
 
