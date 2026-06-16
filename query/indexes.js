77a1e282a70bea56d119084539b4725cbfef1d8a
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-16 15:31:25 +0200

    Add composite index for cuisine and borough

--- query/indexes.js
+++ query/indexes.js
@@ -6,3 +6,4 @@ db.restaurants.createIndex({ name: 1 });
 db.restaurants.createIndex({ "address.street": 1 });
 db.restaurants.createIndex({ "location.coordinates": 1 });
 db.restaurants.createIndex({ location: "2dsphere" });
+db.restaurants.createIndex({ cuisine: 1, borough: -1 });
