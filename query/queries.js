beb43f7a340dc80ab96349156b1378b030ffa331
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-16 14:22:06 +0200

    up to 31

--- query/queries.js
+++ query/queries.js
@@ -80,15 +80,15 @@ db.restaurants.find({}, {_id: 0}).sort({name: -1});
 db.restaurants.find({}, {_id: 0}).sort({cuisine: 1, borough: -1});
 
 // 28. Mostrar direccions que no contenen el carrer.
-db.restaurants.find({"address.street": {$exists: false, $ne: null}}, {_id: 0, address: 1});
+db.restaurants.find({$or: [{"address.street": {$exists: false}}, {"adress.street": null}]]}, {_id: 0, address: 1});
 
 // 29. Seleccionar documents on el valor de `coordinate` és de tipus Double. Mostrar el name, restaurant_id i coordinades.
-
+db.restaurants.find({"location.coordinates": {$elemMatch: {$type: "double" }}}, {_id: 0, restaurant_id: 1, "location.coordinates": 1})
 
 // 30. Mostrar restaurant_id, name i grade per restaurants amb marcador divisible per 7 (resta 0).
 
 
 // 31. Trobar name, borough, longitud, latitud i cuisine per noms que contenen 'mon'.
-
+db.restaurants.find({name: {$regex: "mon", $options: "i"}}, {_id:0, name: 1, borough: 1, "locations.coordinates": 1})
 
 // 32. Mostrar restaurant_id, name i grade i score de més de 80 però menys que 100.
