b389f0df3d75b7a9b2d906bb4fa58bf66865dd92
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-16 11:20:27 +0200

    up to 18

--- query/queries.js
+++ query/queries.js
@@ -50,7 +50,7 @@ db.restaurants.find({name: {$regex: "Reg"}}, {_id: 0, restaurant_id: 1, name: 1,
 db.restaurants.find({borough: "Bronx", cuisine: {$in: ["Chinese", "American"]}}, {_id: 0})
 
 // 18. Trobar restaurant_id, name, borough i cuisine per a Staten Island, Queens, Bronx o Brooklyn.
-
+db.restaurants.find({borough: {$in: ["Staten Island", "Queens", "Bronx", "Brooklyn"]}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1}) 
 
 // 19. Trobar restaurant_id, name, borough i cuisine per a restaurants que NO són d'aquests barris.
 
