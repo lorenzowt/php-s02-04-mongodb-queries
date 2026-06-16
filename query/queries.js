97e47683a65d6c8ebf0435eae63ef92a62bebd31
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-16 13:32:34 +0200

    up to 28

--- query/queries.js
+++ query/queries.js
@@ -62,25 +62,25 @@ db.restaurants.find({"grades.score": {$lte: 10}}, {_id: 0, restaurant_id: 1, nam
 db.restaurants.find({$or : [{cuisine: "Seafood"}, {name: {$regex: "^Wil"}}]}, {_id: 0});
 
 // 22. Trobar restaurant_id, name, i grades per grau "A", score 11, i data "2014-08-11T00:00:00Z".
-db.restaurants.find({grades: {$elemMatch: {grade: "A", score: 11, date: "2014-08-11T00:00:00Z"}}}, {_id: 0, restaurant_id: 1, name: 1, grades: 1});
+db.restaurants.find({grades: {$elemMatch: {grade: "A", score: 11, date: "2014-08-11T00:00:00.000Z"}}}, {_id: 0, restaurant_id: 1, name: 1, grades: 1});
 
 // 23. Trobar restaurant_id, name i grades on el 2n element té grau "A", score 9 i data "2014-08-11T00:00:00Z".
-db.restaurants.find({"grades.1": {grade: "A", score: 9, date: "2014-08-11T00:00:00Z"}}, {_id: 0, restaurant_id: 1, name: 1, grades: 1})
+db.restaurants.find({"grades.1": {grade: "A", score: 9, date: "2014-08-11T00:00:00Z"}}, {_id: 0, restaurant_id: 1, name: 1, grades: 1});
 
 // 24. Trobar el restaurant_id, name, street, zipcode i coordenades dels restaurants a menys de 5 km de [-74, 40.7].
 
 
 // 25. Ordenar els noms dels restaurants en ordre ascendent, mostrant totes les columnes.
-
+db.restaurants.find({}, {_id: 0}).sort({name: 1});
 
 // 26. Ordenar els noms dels restaurants en ordre descendent, mostrant totes les columnes.
-
+db.restaurants.find({}, {_id: 0}).sort({name: -1});
 
 // 27. Ordenar cuisine ascendent i borough descendent.
-
+db.restaurants.find({}, {_id: 0}).sort({cuisine: 1, borough: -1});
 
 // 28. Mostrar direccions que no contenen el carrer.
-
+db.restaurants.find({"address.street": {$exists: false}}, {_id: 0, address: 1});
 
 // 29. Seleccionar documents on el valor de `coordinate` és de tipus Double. Mostrar el name, restaurant_id i coordinades.
 
